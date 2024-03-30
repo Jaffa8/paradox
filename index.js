b0cc2932c2165edac00f21ce81a2a4e90ca543c9
AkhilJ321 <jamwalakhil936@gmail.com>
2024-03-30 20:19:07 +0530

    fixes

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 
-const dotenv = require("dotenv");
 const connectDB = require("./db/index.js");
 const app = require("./app.js");
+require('dotenv').config()
 
-dotenv.config({
-    path: './.env'
-});
 
+console.log('port',process.env.PORT)
 
 const PORT = /*process.env.PORT ||*/ 3001;
 
-connectDB()
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(` Server is running at port : ${PORT}`);
-    });
-})
-.catch((err) => {
-    console.log("MongoDB connection failed: ", err);
-});
+async function startServer() {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is running at port: ${PORT}`);
+        });
+    } catch (err) {
+        console.log("MongoDB connection failed: ", err);
+    }
+}
+
+startServer();
