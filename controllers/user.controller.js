38207f82e87dc76128d67b3114044bdc68450108
Jaffa8 <22dcs021@nith.ac.in>
2024-04-01 21:00:00 +0530

    fixes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,32 +8,35 @@ const createUser = async (req, res) => {
       name,
       email,
       roll,
-     
+      refCode,
+      teamCode,
+      teamName,
       displayPicture,
     } = req.body;
 
     
     const existingUser = await ParadoxUser.findOne({ uid });
 
-    if (!existingUser) {
-      return res.status(404).json({ message: "User not found", success: false });
+    if (existingUser) {
+      return res.status(404).json({ message: "User already exists", success: false });
     }
 
    
-    const userProfile = {
-     
-      name: name,
-      email: email,
-      roll: roll,
-      refCode: refCode,
-      teamCode: teamCode,
-      teamName: teamName,
-      displayPicture: displayPicture,
-    };
-
-   
-    existingUser.profile = userProfile;
-    await existingUser.save();
+    const newUser = new ParadoxUser({
+      uid,
+      name, 
+      email: 
+      roll,
+      refCode,
+      teamCode,
+      teamName,
+      image:displayPicture,
+      currQues:1,
+      score:0,
+      level:1,
+    });
+  
+   await newUser.save();
 
     return res.status(200).json({ message: "User profile created successfully", success: true });
   } catch (error) {
