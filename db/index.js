8ece1efbc279bd1885f710341df1c713adfe2cf1
Jaffa8 <22dcs021@nith.ac.in>
2024-04-01 19:03:58 +0530

    mongo

--- db/index.js
+++ db/index.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const DB_NAME=require("../constant")
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
